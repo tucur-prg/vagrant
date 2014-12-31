af3b5c46917678cd0841dc56b64e76d737f490ea
tucur-prg <tucur.prg+github@gmail.com>
2014-12-31 21:41:18 +0900

    add docker

---
+++ chef-repo/cookbooks/setup/recipes/docker.rb
@@ -0,0 +1,15 @@
+
+%w{
+  docker-io
+}.each do |pkg|
+    yum_package "#{pkg}" do
+    end
+end
+
+%w{
+  docker
+}.each do |pkg|
+    service "#{pkg}" do
+        action [ :enable, :start ]
+    end
+end

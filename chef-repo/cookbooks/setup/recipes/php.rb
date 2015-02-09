1febbfdcd37b3ba28488197e2cce4ef6c40f9edf
tucur-prg <tucur.prg+github@gmail.com>
2015-02-09 14:52:05 +0900

    fix php

--- chef-repo/cookbooks/setup/recipes/php.rb
+++ chef-repo/cookbooks/setup/recipes/php.rb
@@ -1,4 +1,42 @@
 
+remote_file "#{Chef::Config[:file_cache_path]}/remi-release-6.rpm" do
+    action :create
+    source "http://rpms.famillecollet.com/enterprise/remi-release-6.rpm"
+    not_if "rpm -qa | grep -q '^remi-release'"
+    notifies :install, "rpm_package[remi-release]", :immediately
+end
+
+rpm_package "remi-release" do
+    action :nothing
+    source "#{Chef::Config[:file_cache_path]}/remi-release-6.rpm"
+end
+
+# phpenv + php-build
+%w{
+  libxml2
+  libxml2-devel
+  curl-devel
+  libjpeg-devel
+  libpng-devel
+  libmcrypt-devel
+  mysql-devel
+  readline-devel
+  libtidy-devel
+  libxslt-devel
+}.each do |pkg|
+    yum_package "#{pkg}" do
+    end
+end
+
+bash "phpenv php-build" do
+    code <<-EOS
+        curl https://raw.githubusercontent.com/CHH/phpenv/master/bin/phpenv-install.sh | bash
+        git clone git://github.com/CHH/php-build.git ~/.phpenv/plugins/php-build
+        echo 'export PATH="$HOME/.phpenv/bin:$PATH"' >> ~/.bashrc
+        echo 'eval "$(phpenv init -)"' >> ~/.bashrc
+    EOS
+end
+
 # PHP Version 5.5.x
 yum_package "php" do
     options '--enablerepo=remi,remi-php55'

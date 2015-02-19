f6d1b10fb5b52edb920ce5a97557035372eb4d85
github <github@localhost.localdomain>
2015-02-19 16:49:00 +0900

    fix apache

--- chef-repo/cookbooks/setup/recipes/apache.rb
+++ chef-repo/cookbooks/setup/recipes/apache.rb
@@ -1,10 +1,28 @@
 
 yum_package "httpd" do
     notifies :install, "yum_package[mod_ssl]", :immediately
+    notifies :create, "template[/etc/httpd/conf/httpd.conf]", :immediately
 end
 
 yum_package "mod_ssl" do
+    notifies :create, "template[/etc/httpd/conf.d/ssl.conf]", :immediately
     notifies :restart, "service[httpd]"
+end
+
+template "/etc/httpd/conf/httpd.conf" do
+    action :nothing
+    source "apache.httpd.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+end
+
+template "/etc/httpd/conf.d/ssl.conf" do
+    action :nothing
+    source "apache.ssl.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
 end
 
 %w{

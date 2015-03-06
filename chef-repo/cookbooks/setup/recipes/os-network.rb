e0ad1a3c1ab821d42a2aac347d9e46fd61e8a38e
shirasaki-norimi <shirasaki-norimi@norimitsu-shirasaki.local>
2015-03-06 16:34:45 +0900

    fix os network

---
+++ chef-repo/cookbooks/setup/recipes/os-network.rb
@@ -0,0 +1,11 @@
+
+cookbook_file "/etc/sysconfig/network-script/ifcfg-eth0" do
+    source "network-script.ifcfg-eth0"
+    owner "root"
+    group "root"
+    mode 0644
+end
+
+service "network" do
+    action [ :restart ]
+end

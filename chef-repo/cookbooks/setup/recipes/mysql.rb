31a022f6ae95cfc77394c8bfdf84bd005d13be88
shirasaki-norimi <shirasaki-norimi@norimitsu-shirasaki.local>
2015-03-25 18:47:46 +0900

    fix mysql replication

--- chef-repo/cookbooks/setup/recipes/mysql.rb
+++ chef-repo/cookbooks/setup/recipes/mysql.rb
@@ -1,31 +1,31 @@
 
 remote_file "#{Chef::Config[:file_cache_path]}/mysql-community-release-el6-5.noarch.rpm" do
-    source "http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm"
-    not_if "rpm -qa | grep -q '^mysql-community-release'"
-    action :create
-    notifies :install, "rpm_package[mysql-community-release]", :immediately
+  source "http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm"
+  not_if "rpm -qa | grep -q '^mysql-community-release'"
+  action :create
+  notifies :install, "rpm_package[mysql-community-release]", :immediately
 end
 
 rpm_package "mysql-community-release" do
-    source "#{Chef::Config[:file_cache_path]}/mysql-community-release-el6-5.noarch.rpm"
-    action :nothing
+  source "#{Chef::Config[:file_cache_path]}/mysql-community-release-el6-5.noarch.rpm"
+  action :nothing
 end
 
 %w{
-    mysql
-    mysql-devel
-    mysql-server
-    mysql-utilities
+  mysql
+  mysql-devel
+  mysql-server
+  mysql-utilities
 }.each do |pkg|
-    yum_package "#{pkg}" do
-        options '--enablerepo=mysql-tools-community'
-    end
+  yum_package "#{pkg}" do
+    options '--enablerepo=mysql-tools-community'
+  end
 end
 
 %w{
-    mysqld
+  mysqld
 }.each do |pkg|
-    service "#{pkg}" do
-        action [ :enable, :start ]
-    end
+  service "#{pkg}" do
+    action [ :enable, :start ]
+  end
 end

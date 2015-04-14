cc82c8c1e2d1aafe7d1f2fb3fb1cfe333c5cc609
tucur-prg <tucur.prg+github@gmail.com>
2015-04-14 12:34:28 +0900

    add capistrano

---
+++ chef-repo/cookbooks/setup/attributes/default.rb
@@ -0,0 +1,7 @@
+
+default[:rbenv] = {
+  :ruby => {
+    :versions => ["2.1.2"],
+    :global => "2.1.2"
+  }
+}
--- chef-repo/cookbooks/ubuntu_setup/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'ubuntu_setup'
-maintainer       'none'
-maintainer_email 'tucur.prg+github@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures ubuntu_setup'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'

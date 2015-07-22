d5315b2a14768ef6ebee455d5d15f9c4e92c0a6f
shirasaki-norimi <shirasaki-norimi@norimitsu-shirasaki.local>
2015-07-22 18:46:08 +0900

    fixed couchbase scripts

--- couchbase.vb.ns.com/scripts/example/insert.py
+++ couchbase.vb.ns.com/scripts/example/insert.py
@@ -6,6 +6,9 @@ from couchbase.exceptions import CouchbaseError
 c = Bucket('couchbase://localhost/default')
 
 try:
+    for i in range(100):
+        c.upsert('number' + str(i), { 'num': i, 'group': i % 3, 'name': 'hoge' })
+
     c.upsert('hoge', { 'some': 'value', 'some2': 'value2' })
 
     rv = c.get('hoge')
--- couchbase.vb.ns.com/scripts/example/query.py
+++ couchbase.vb.ns.com/scripts/example/query.py
@@ -3,15 +3,11 @@
 from couchbase.bucket import Bucket
 from couchbase.exceptions import CouchbaseError
 
-c = Bucket('couchbase://localhost/beer-sample')
+c = Bucket('couchbase://localhost/default')
 
 try:
-    for row in c.query('beer', 'brewery_beers', limit=3):
-        print row.key
-
-        result = c.get(row.key[0])
-        print result.value
-
+    for row in c.query('example', 'person', limit=5, skip=0, startkey=[0, 10], endkey=[0, 40], descending=False):
+        print unicode("{key}: {value}").format(key=row.key, value=row.value)
 
 except CouchbaseError as e:
     print e

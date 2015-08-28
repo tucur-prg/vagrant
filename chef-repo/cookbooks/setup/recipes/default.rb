f441e13385d8d03301f0c73296caafed51090a47
shirasaki-norimi <shirasaki-norimi@norimitsu-shirasaki.local>
2015-08-28 11:51:18 +0900

    add java tomcat and build

--- chef-repo/cookbooks/setup/recipes/default.rb
+++ chef-repo/cookbooks/setup/recipes/default.rb
@@ -1,22 +1,8 @@
 
-### Settings #####################################
-
-# Yum Repo EPEL
-remote_file "#{Chef::Config[:file_cache_path]}/epel-release-6.8.noarch.rpm" do
-  action :create
-  source "http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
-  not_if "rpm -qa | grep -q '^epel-release'"
-  notifies :install, "rpm_package[epel-release]", :immediately
-end
-
-rpm_package "epel-release" do
-  action :nothing
-  source "#{Chef::Config[:file_cache_path]}/epel-release-6.8.noarch.rpm"
-end
-
 ### Software Install #############################
 
 %w{
+  epel-release
   wget
   openssl-devel
   sysstat

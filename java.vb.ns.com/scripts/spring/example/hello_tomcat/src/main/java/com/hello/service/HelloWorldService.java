cd2546c6e92176de7bf9668d1f716eeab2e3af03
shirasaki-norimi <shirasaki-norimi@norimitsu-shirasaki.local>
2015-09-29 17:50:43 +0900

    add java spring mvc example

---
+++ java.vb.ns.com/scripts/spring/example/hello_tomcat/src/main/java/com/hello/service/HelloWorldService.java
@@ -0,0 +1,27 @@
+package com.hello.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+public class HelloWorldService {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldService.class);
+
+    public String getDesc() {
+        logger.debug("getDesc() is executed!");
+
+        return "Gradle + Spring MVC HelloWorld Example";
+    }
+
+    public String getTitle(String name) {
+        logger.debug("getTitle() is executed! $name : {}", name);
+
+        if (StringUtils.isEmpty(name)) {
+            return "Hello World!";
+        } else {
+            return "Hello " + name;
+        }
+    }
+}

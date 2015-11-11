6e1e7fb693e0f393705998ed2dde43390d4fbddb
tucur-prg <tucur.prg+github@gmail.com>
2015-11-11 17:29:17 +0900

    fixed couchbase4

--- chef-repo/cookbooks/setup/recipes/couchbase4-initialize.rb
+++ chef-repo/cookbooks/setup/recipes/couchbase4-initialize.rb
@@ -1,9 +1,10 @@
 
 bash "cluster-init" do
   code <<-EOH
-    /opt/couchbase/bin/couchbase-cli cluster-init -c #{node['couchbase']['first']}:8091 \
-         --user=#{node['couchbase']['admin']['user']} \
-         --password=#{node['couchbase']['admin']['password']} \
+    /opt/couchbase/bin/couchbase-cli cluster-init \
+         -c #{node['couchbase']['first']}:8091 \
+         -u #{node['couchbase']['admin']['user']} \
+         -p #{node['couchbase']['admin']['password']} \
          --cluster-username=#{node['couchbase']['admin']['user']} \
          --cluster-password=#{node['couchbase']['admin']['password']} \
          --cluster-ramsize=#{node['couchbase']['ramsize']} \
@@ -14,9 +15,10 @@ end
 
 bash "node-init" do
   code <<-EOH
-    /opt/couchbase/bin/couchbase-cli node-init -c #{node['couchbase']['first']}:8091 \
-         --user=#{node['couchbase']['admin']['user']} \
-         --password=#{node['couchbase']['admin']['password']} \
+    /opt/couchbase/bin/couchbase-cli node-init \
+         -c #{node['couchbase']['first']}:8091 \
+         -u #{node['couchbase']['admin']['user']} \
+         -p #{node['couchbase']['admin']['password']} \
          --node-init-data-path=/opt/couchbase/var/lib/couchbase/data \
          --node-init-index-path=/opt/couchbase/var/lib/couchbase/data \
          --node-init-hostname=#{node['couchbase']['first']}

ee5a9518531b4d4203d2e9ba2fde473e4f3104a5
tucur-prg <tucur.prg+github@gmail.com>
2016-02-05 13:10:33 +0900

    fixed php52

--- chef-repo/cookbooks/setup/recipes/apache.rb
+++ chef-repo/cookbooks/setup/recipes/apache.rb
@@ -3,6 +3,9 @@ yum_package "httpd" do
   notifies :install, "yum_package[mod_ssl]", :immediately
 end
 
+yum_package "httpd-devel" do
+end
+
 yum_package "mod_ssl" do
   notifies :create, "template[/etc/httpd/conf.d/ssl.conf]", :immediately
   notifies :restart, "service[httpd]"
@@ -10,23 +13,13 @@ end
 
 template "/etc/httpd/conf/httpd.conf" do
   source "apache.httpd.conf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
 end
 
 template "/etc/httpd/conf.d/ssl.conf" do
   action :nothing
   source "apache.ssl.conf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
 end
 
-%w{
-  httpd
-}.each do |pkg|
-  service "#{pkg}" do
-    action [ :enable, :start ]
-  end
+service "httpd" do
+  action [ :enable, :start ]
 end

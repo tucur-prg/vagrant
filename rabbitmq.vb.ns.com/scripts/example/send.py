7de0ef3a44ee83b9014796155fe7d6394ff057b7
tucur-prg <tucur.prg+github@gmail.com>
2016-02-23 15:29:52 +0900

    fixed php curl

--- rabbitmq.vb.ns.com/scripts/example/send.py
+++ rabbitmq.vb.ns.com/scripts/example/send.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import pika
+import time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
@@ -7,9 +8,13 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
-print " [x] Sent 'Hello World!'"
+num = 0
+while num < 100000:
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body='Hello World! ' + str(num))
+    print " [x] Sent 'Hello World!' " + str(num)
+    num += 1
+#    time.sleep(0.001)
 
 connection.close()
